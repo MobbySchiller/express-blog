dfb67a714ffc27d8a117bcf75eb6b519c979b08e
MobbySchiller <kb.burzynski@gmail.com>
2022-12-19 14:15:24 +0100

    add admin login and admin news

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const News = require('../models/news')
 const router = express.Router();
+
+router.all('*', (req, res, next) => {
+    if (!req.session.admin) {
+        res.redirect('login')
+        return
+    }
+
+    next()
+})
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Admin' });
+    News.find({}, (err, data) => {
+        console.log(data)
+        res.render('admin/index', { title: 'Admin', data });
+    })
 });
+
+router.route('/news/add')
+    .get((req, res) => {
+        res.render('admin/news-form', { title: 'Dodaj news', body: {}, errors: {} })
+    })
+    .post((req, res) => {
+        const body = req.body
+        const newsData = new News(body)
+        const errors = newsData.validateSync()
+        newsData.save((err) => {
+            if (err) {
+                res.render('admin/news-form', { title: 'Dodaj news', errors, body })
+                return
+            }
+            res.redirect('/admin')
+        })
+    })
+
+router.get('/news/delete/:id', (req, res) => {
+    News.findByIdAndDelete(req.params.id, (err) => {
+        res.redirect('/admin')
+    })
+})
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const login = 'admin1'
+const password = 'admin2'
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'Logowanie' });
+});
+
+router.post('/login', (req, res) => {
+  const body = req.body
+
+  if (login === body.login && password === body.password) {
+    req.session.admin = 1
+    res.redirect('/admin')
+  } else {
+    res.redirect('/login')
+  }
+})
 
 module.exports = router;
